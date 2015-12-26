8246af5ddf34a134b4f318ebe3325ddcb3487279
Radoslav Stankov <rstankov@gmail.com>
2015-12-26 15:49:06 +0200

    Initial commit

---
+++ RDSAnnotatedLabel/RDSAnnotatedLabel.h
@@ -0,0 +1,19 @@
+//
+//  RDSAnnotatedLabel.h
+//  RDSAnnotatedLabel
+//
+//  Created by Radoslav Stankov on 12/26/15.
+//  Copyright © 2015 Radoslav Stankov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RDSAnnotatedLabel.
+FOUNDATION_EXPORT double RDSAnnotatedLabelVersionNumber;
+
+//! Project version string for RDSAnnotatedLabel.
+FOUNDATION_EXPORT const unsigned char RDSAnnotatedLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RDSAnnotatedLabel/PublicHeader.h>
+
+
